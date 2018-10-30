7bf4c4595c506806494930c8d971d3c4b27e0594
Anwesh43 <anweshthecool0@gmail.com>
2018-10-30 11:13:41 +0530

    adding GateShapeStepView in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedgateshapestepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedgateshapestepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedgateshapestepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.gateshapestepview.GateShapeStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        GateShapeStepView.create(this)
     }
 }
