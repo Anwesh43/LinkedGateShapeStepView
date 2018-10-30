192c35f5c0c68b025ca708b96b75e5c740ffdc9a
Anwesh43 <anweshthecool0@gmail.com>
2018-10-30 10:53:55 +0530

    adding GateShapeStepView class

--- gateshapestepview/src/main/java/com/anwesh/uiprojects/gateshapestepview/GateShapeStepView.kt
+++ gateshapestepview/src/main/java/com/anwesh/uiprojects/gateshapestepview/GateShapeStepView.kt
@@ -38,3 +38,21 @@ fun Canvas.drawGSSNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class GateShapeStepView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
