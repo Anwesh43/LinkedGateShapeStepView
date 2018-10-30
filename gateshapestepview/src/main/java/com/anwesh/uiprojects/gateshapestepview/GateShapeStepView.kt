ea95185a56d778386e3a503897f1ae6c528adc63
Anwesh43 <anweshthecool0@gmail.com>
2018-10-30 11:11:54 +0530

    using Renderer in View

--- gateshapestepview/src/main/java/com/anwesh/uiprojects/gateshapestepview/GateShapeStepView.kt
+++ gateshapestepview/src/main/java/com/anwesh/uiprojects/gateshapestepview/GateShapeStepView.kt
@@ -45,14 +45,16 @@ class GateShapeStepView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas, paint)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
@@ -205,7 +207,7 @@ class GateShapeStepView(ctx : Context) : View(ctx) {
         fun create(activity : Activity) : GateShapeStepView {
             val view : GateShapeStepView = GateShapeStepView(activity)
             activity.setContentView(view)
-            return view 
+            return view
         }
     }
 }
